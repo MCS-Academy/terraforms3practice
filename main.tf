985cc75962a19ae2362edfdd4a097c13ef073162
Ewere Diagboya <zodiacten@hotmail.com>
2021-05-09 21:02:38 +0100

    change db table name

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "mcsbucket" {
 }
 
 resource "aws_dynamodb_table" "basic-dynamodb-table" {
-  name           = "MCSGameScores"
+  name           = "MyCloudSeriesGameScores"
   billing_mode   = "PROVISIONED"
   read_capacity  = 20
   write_capacity = 20
